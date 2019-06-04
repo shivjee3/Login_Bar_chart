dd7b13b7051080fae5053734badd53f6aa18110e
shiv <shivjeejaiswal9@gmail.com>
2019-06-04 23:49:54 +0530

    Updates

---
+++ precache-manifest.bf14e036a609463b822ebf43216e23c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24aab189356cdcf54c2601ccc0309492",
+    "url": "/Login_Bar_chart/index.html"
+  },
+  {
+    "revision": "9f643aa9561cf06fefae",
+    "url": "/Login_Bar_chart/static/css/main.1ab2b700.chunk.css"
+  },
+  {
+    "revision": "0d4dab36d36a5f2480b1",
+    "url": "/Login_Bar_chart/static/js/2.467f3520.chunk.js"
+  },
+  {
+    "revision": "9f643aa9561cf06fefae",
+    "url": "/Login_Bar_chart/static/js/main.0c145ad2.chunk.js"
+  },
+  {
+    "revision": "bae49d113bc6053f4ad9",
+    "url": "/Login_Bar_chart/static/js/runtime~main.a0967f45.js"
+  },
+  {
+    "revision": "421c33a0569cf7887d3c0ad9f88e74ce",
+    "url": "/Login_Bar_chart/static/media/backimg.421c33a0.jpg"
+  }
+]);
